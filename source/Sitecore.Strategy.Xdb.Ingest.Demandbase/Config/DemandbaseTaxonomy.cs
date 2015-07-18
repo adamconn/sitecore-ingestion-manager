b4148412cd0f97df64e6f22e48feee06a2093925
Edvin Eshagh <ees@sitecore.net>
2015-07-17 17:44:36 -0700

    1) Moved all project references to a common folder within the solution 2) Added Demandbase Taxonomy to get ContactFacetMemberValues

---
+++ source/Sitecore.Strategy.Xdb.Ingest.Demandbase/Config/DemandbaseTaxonomy.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+using Sitecore.Xml;
+
+namespace Sitecore.Strategy.Xdb.Ingest.Demandbase.Config
+{
+    public class DemandbaseTaxonomy
+    {
+
+        public class FacetMemberValue
+        {
+            public string Description { get; set; }
+            public string Value { get; set; }
+        }
+        
+        public DemandbaseTaxonomy()
+        {
+            MemberValues = new Dictionary<string, List<FacetMemberValue>>();
+        }
+
+
+        public Dictionary<string, List<FacetMemberValue>> MemberValues { get; private set; }
+
+        public void AddItemName(XmlNode node)
+        {
+            var facetMemberName = XmlUtil.GetAttribute("name", node);
+            if (string.IsNullOrEmpty(facetMemberName))
+            {
+                return;
+            }
+
+            var facetMemberList = new List<FacetMemberValue>();
+
+            foreach (XmlNode childNode in node.ChildNodes)
+            {
+                var desc = XmlUtil.GetAttribute("description", childNode);
+                var value = XmlUtil.GetAttribute("value", childNode);
+
+                if (string.IsNullOrWhiteSpace(desc)) desc=value;
+                if (string.IsNullOrWhiteSpace(value)) value = desc;
+
+                if (!string.IsNullOrWhiteSpace(value) 
+                &&  !string.IsNullOrWhiteSpace(value))
+                {
+                    facetMemberList.Add(new FacetMemberValue() {Description = desc, Value = value});
+                }
+            }
+
+            MemberValues.Add(facetMemberName,facetMemberList);
+
+        }
+    }
+}
---
+++ source/Sitecore.Strategy.Xdb.Ingest.Demandbase/Pipelines/GetFacetMemberValues.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Configuration;
+using Sitecore.Diagnostics;
+using Sitecore.Strategy.Contacts.DataProviders;
+using Sitecore.Strategy.Contacts.Pipelines.GetFacetMemberValues;
+using Sitecore.Strategy.Xdb.Ingest.Demandbase.Config;
+using Sitecore.Strategy.Xdb.Ingest.Demandbase.Models;
+
+namespace Sitecore.Strategy.Xdb.Ingest.Demandbase.Pipelines
+{
+    public class GetFacetMemberValues
+    {
+        public virtual void Process(GetFacetMemberValuesArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            Assert.ArgumentNotNullOrEmpty(args.FacetName, "args.FacetName");
+
+            var type = ContactFacetHelper.GetContractTypeForFacet(args.FacetName);
+
+            if (type == null
+            || !typeof(IDemandbaseData).IsAssignableFrom(type))
+            {
+                return;
+            }
+
+            var taxonomy = Factory.CreateObject("demandbaseTaxonomy", true) as DemandbaseTaxonomy;
+            if (taxonomy == null
+            || (!taxonomy.MemberValues.ContainsKey(args.MemberName)))
+            {
+                return;
+            }
+
+            taxonomy
+                .MemberValues[args.MemberName]
+                .ForEach( m => args.Values.Add(m.Description, m.Value));
+
+        }
+
+    }
+}
